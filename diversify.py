9797de56507d044e7f3eada6c0c20d2c9dc19a01
Bryan Lin <bryanlin850@gmail.com>
2017-12-05 01:46:26 -0600

    Comments

--- diversify.py
+++ diversify.py
@@ -16,6 +16,13 @@ artist_name = "Charlie Puth"
 
 
 def lyrics_from_song_api_path(song_api_path):
+    """
+    Find lyrics given an API path
+    @params:
+        song_api_path, endpoint to the desired song
+    @return:
+        lyrics, a string of all the lyrics
+    """
     song_url = base_url + song_api_path
     response = requests.get(song_url, headers=headers)
     json = response.json()
@@ -33,8 +40,10 @@ def lyrics_from_song_api_path(song_api_path):
 
 
 if __name__ == "__main__":
+    # search URL is http://api.genius.com/search
     search_url = base_url + "/search"
     data = {'q': song_title}
+    # putting together the request for the requests library
     response = requests.get(search_url, params=data, headers=headers)
     json = response.json()
     song_info = None
@@ -47,17 +56,40 @@ if __name__ == "__main__":
         song_api_path = song_info["result"]["api_path"]
         lyrics = lyrics_from_song_api_path(song_api_path)
         lyrics += '[]'
+
+    # q is the query
+    # After Alexa integration, the query will be input by the user with voice input.
+    # For now, the query is specified in the code or through the command line.
     q = 'Verse 1'
+    # split the lyrics by the '[' character
     lyrics = lyrics.split('[')
-    lyrics = lyrics[:-1]
+    # take everything but the last character (which will always be ']')
+    lyrics = lyrics[: -1]
     base_lyr = ''
     for sect in lyrics:
+        # search the Genius lyrics for the desired section
         if sect.startswith(q):
             base_lyr = sect
             break
+    
+    """
+    The lyrics in their current form look something like this:
+        Verse 1]\n
+        You've been runnin' 'round, runnin' 'round, runnin' 'round\n
+        Throwin' that dirt all on my name\n
+        ...
+    So we need to strip the extra ']' while keeping the timestamp somehow separate
+    from the text. The ']' provides a convenient character to split each string on.
+    The end result is a 2-element list of strings where the first element is the 
+    timestamp and the second element is the lyrics for that section.
+    We throw away the first element and split the second section on the '\n'
+    character, and creating a list of strings where each element is a different
+    line of the lyrics.
+    """
     base_lyr = base_lyr.split(']')
     base_lyr = base_lyr[1]
     base_lyr = base_lyr.split('\n')
+    # clean up the lyrics by removing empty elements
     base_lyr[:] = [item for item in base_lyr if item]
     time_name = ''
     if song_title is 'Attention':
@@ -70,18 +102,51 @@ if __name__ == "__main__":
     time = ''
     with open(time_name, 'r') as f:
         time_lyr = f.read()
+        """
+        The timed lyrics in their current form look something like this:
+            [00:04.32]Woah-oah, hm-hmm
+            \n\n
+            [00:09.58]You've been runnin' 'round, runnin' 'round, runnin' 'round\n
+            [00:11.69]Throwin' that dirt all on my name\n
+            ...
+        Note that there are two '\n' characters between sections. We take 
+        advantage of this and split the string on occurences of '\n\n' to get
+        a list of strings where each element is a different section of the song.
+        """
         time_lyr = time_lyr.split('\n\n')
+        """
+        The timed lyrics now look something like this:
+            [
+                "[00:04.32]Woah-oah, hm-hmm",
+                "[00:09.58]You've been runnin' 'round, runnin' 'round, runnin' 'round"
+                "[00:11.69]Throwin' that dirt all on my name"
+                ...
+            ]
+        Each element is a different section of the song as previous.
+        This list comprehension replaces every '\n' with '' and also splits
+        each section on the '[' character, turning the Verse 1 section, for
+        example, into:
+            [
+                "00:09.58]You've been runnin' 'round, runnin' 'round, runnin' 'round",
+                "00:11.69]Throwin' that dirt all on my name",
+                ...
+            ]
+        Once again, splitting on the ']' character gives a conveniently separated 
+        list that looks like [[timestamp1, line1], [timestamp2, line2], ...]
+        The lines will be searched line by line to see if it matches with the lyrics
+        query and the timestamp will be returned if it does match.
+        """
         time_lyr[:] = [item.replace('\n', '').split('[') for item in time_lyr]
-        time_lyr[:] = [[item.split(']') for item in para[1:]]
-                       for para in time_lyr]
+        time_lyr[:] = [[item.split(']') for item in sect[1:]]
+                       for sect in time_lyr]
         found_bool = False
-        for para in time_lyr:
+        for sect in time_lyr:
             if found_bool:
                 break
             i = 0
-            for line in para:
+            for line in sect:
                 if line[1] == base_lyr[i]:
-                    time = para[0][0]
+                    time = sect[0][0]
                     i += 1
                     if i == len(base_lyr):
                         found_bool = True
